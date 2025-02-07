d62d016edd067e159ecc09c99a20e32b41b0af41
Marshall Wice <mhwice@protonmail.com>
2025-02-06 22:36:17 -0800

    add register page ui and finish login ui, setup login and register server actions

---
+++ actions/login.ts
@@ -0,0 +1,10 @@
+"use server";
+
+import { z } from "zod";
+import { LoginSchema } from "@/schema";
+
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+  const validatedFields = LoginSchema.safeParse(values);
+  if (!validatedFields.success) return { error: "Invalid fields" };
+  return { success: "Email sent"}
+}
