4d4f0c76074b4cd7f4fb5ad5d61120b27e1af9c0
Marshall Wice <mhwice@protonmail.com>
2025-03-10 14:21:02 -0700

    feat: add pg migration file to create links table

---
+++ migrations/1741638115166_table-links.js
@@ -0,0 +1,39 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.sql(`
+    CREATE TABLE links (
+      id SERIAL PRIMARY KEY,
+      original_url VARCHAR(255) NOT NULL,
+      short_url VARCHAR(63) NOT NULL UNIQUE,
+      code VARCHAR(15) NOT NULL UNIQUE,
+      link_clicks INTEGER DEFAULT 0 NOT NULL CHECK(link_clicks >= 0),
+      qr_clicks INTEGER DEFAULT 0 NOT NULL CHECK(link_clicks >= 0),
+      created_at TIMESTAMPTZ DEFAULT now() NOT NULL,
+      updated_at TIMESTAMPTZ DEFAULT now() NOT NULL,
+      user_id TEXT NOT NULL REFERENCES "user"(id) ON DELETE CASCADE,
+      expiration_date TIMESTAMPTZ,
+      password VARCHAR(63),
+      CHECK (updated_at >= created_at)
+    );
+  `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.sql(`
+    DROP TABLE links;
+  `);
+};
