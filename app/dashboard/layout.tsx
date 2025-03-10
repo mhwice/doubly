4ab0135647fb79f5cefc491fe31a448fd0e03bf6
Marshall Wice <mhwice@protonmail.com>
2025-03-10 09:58:29 -0700

    sidebar tests

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -16,7 +16,23 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
 
 /*
 
-We need a layout to contain the sidebar.
-Then each page in the sidebar 'links', 'analytics', etc. can be a child of the sidebar.
+Todo:
+- under the 'links' page I want to see a table with links (use dummy data),
+  filter and display options, a search bar pagination, and a create link button
+- under the 'analytics' page i need to show something.... but what?
+
+- the location of the user (city/country/region/continent) [how do i do this???]
+- maybe a graph that shows link clicks over time
+- link type (qr vs link)
+- browser type, etc.
+- suppose for now, just fill the page with some generic analytic cards.
+
+I like the data table from shadcn examples.
+
+I could have it where you can filter over your urls and whichever urls you select the stats
+reflect only those urls. that way we can easily see the stats for any url.
+
+
+
 
 */
