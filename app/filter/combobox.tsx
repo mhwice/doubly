556e8e49089df8994321d9bd165e2e45a5bd3ab6
Marshall Wice <mhwice@protonmail.com>
2025-03-23 14:19:59 -0700

    feat: combobox now displays deeply nested menus, handles selections and inputs, and displays badges when item selected

--- app/filter/combobox.tsx
+++ app/filter/combobox.tsx
@@ -20,45 +20,14 @@ import {
 } from "@/components/ui/popover"
 
 import { data as filterFields } from "./data";
-
-// const filterFields = [
-//   {
-//     value: "a",
-//     label: "a",
-//     page: "a",
-//     options: [
-//       { value: "apples", label: "apples" },
-//       { value: "apricots", label: "apricots" },
-//       { value: "anchovies", label: "anchovies" },
-//     ]
-//   },
-//   {
-//     value: "b",
-//     label: "b",
-//     page: "b",
-//     options: [
-//       { value: "bacon", label: "bacon" },
-//       { value: "bananas", label: "bananas" },
-//     ]
-//   },
-//   {
-//     value: "c",
-//     label: "c",
-//     page: "c",
-//     options: [
-//       { value: "candy", label: "candy" }
-//     ]
-//   },
-//   {
-//     value: "d",
-//     label: "d",
-//   }
-// ];
+import { Badge } from "@/components/ui/badge"
 
 export function Combobox() {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
   const [page, setPage] = useState<string>("root");
+  const [selectedValues, setSelectedValues] = useState<string[]>([]);
 
   const renderMainMenu = () => {
     const menu = findSubmenu(page, filterFields);
@@ -69,8 +38,13 @@ export function Combobox() {
             return (
               <CommandItem key={field.label} onSelect={(val) => {
                   if (field.sub === undefined) {
-                    console.log("selected", field.label)
+                    // console.log("selected", field.label)
+                    setSelectedValues((currVals) => {
+                      if (currVals.includes(field.label)) return currVals;
+                      return [...currVals, field.label];
+                    })
                   } else {
+                    setInputValue("");
                     setValue(val);
                     setPage(field.label);
                   }
@@ -91,22 +65,58 @@ export function Combobox() {
   }
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button variant="outline" role="combobox" aria-expanded={open} className="w-[200px] justify-between" >
-          Filter
-          <ChevronsUpDown className="opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command>
-          <CommandInput placeholder="search..." className="h-9" />
-          {renderMainMenu()}
-        </Command>
-      </PopoverContent>
-    </Popover>
+    <div className="flex flex-col">
+      <div className="w-[200px]">
+        {selectedValues.map((item) => (
+          <Badge
+            key={item}
+            variant="outline"
+            style={badgeStyle("#ef4444")}
+            className="mb-2 mr-2"
+          >
+            {item}
+          </Badge>
+        ))}
+      </div>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button variant="outline" role="combobox" aria-expanded={open} className="w-[200px] justify-between" >
+            Filter
+            <ChevronsUpDown className="opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandInput
+              placeholder="search..."
+              className="h-9"
+              value={inputValue}
+              onValueChange={(e) => {
+                setInputValue(e);
+              }}
+              // onKeyDown={(e) => {
+              //   console.log("onKeyDown", e);
+              // }}
+              // onBlur={(e) => {
+              //   console.log("onBlur", e);
+              // }}
+              // onInput={(e) => {
+              //   console.log("onInput", e);
+              // }}
+              />
+            {renderMainMenu()}
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </div>
   )
 }
+
+const badgeStyle = (color: string) => ({
+  borderColor: `${color}20`,
+  backgroundColor: `${color}30`,
+  color,
+});
 
 type MenuItem = {
   label: string;
