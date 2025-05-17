5cee684b5bc1d6f5306a7bafa520ab1f0886859f
Marshall Wice <mhwice@protonmail.com>
2025-05-17 13:25:15 -0700

    feat: update queue handler to write events to db, clean up click schemas

--- lib/zod/clicks.ts
+++ lib/zod/clicks.ts
@@ -1,5 +1,4 @@
 import { z } from 'zod';
-import { LinkSchemas } from './links';
 import { snakeCase } from 'change-case';
 
 // TODO - check what happens if I insert a lat/lng into the db with too much precision
@@ -20,9 +19,16 @@ const ClickEventSchema = z.object({
   os: z.string().trim().min(1),
 });
 
-// const ClickEventCreateSchema = ClickEventSchema.omit({
-//   id: true
-// });
+export const ClickRecord = ClickEventSchema.omit({
+  id: true,
+  createdAt: true
+}).extend({
+  createdAt: z.string()
+}).transform((data) => {
+  return Object.fromEntries(
+    Object.entries(data).filter(([_, value]) => value !== undefined).map(([key, value]) => [snakeCase(key), value])
+  );
+});
 
 const ClickEventCreateSchema = ClickEventSchema.omit({
   id: true
@@ -32,16 +38,10 @@ const ClickEventCreateSchema = ClickEventSchema.omit({
   );
 });
 
-// type snack = z.infer<typeof ClickEventCreateSchemaWithTransform>;
-
-// const ClickEventGetAllSchema = ClickEventSchema.pick({
-//   linkId: true
-// });
-
 const ClickFilterSchema = z.object({
-  field: z.string(), //z.enum(["source", "continent", "country", "city", "originalUrl", "shortUrl"]),
-  value: z.string(),//.trim().min(1).max(255).optional(),
-  count: z.number()//z.preprocess(Number, z.number())
+  field: z.string(),
+  value: z.string(),
+  count: z.number()
 })
 
 const ClickChartSchema = z.object({
@@ -50,19 +50,7 @@ const ClickChartSchema = z.object({
   linkCount: z.number()
 })
 
-// const FilterRepsonseSchema = z.object({
-//   filter: ClickFilterSchema.array(),
-//   chart: ClickChartSchema.array(),
-// });
-
 const JSONEntitySchema = z.object({
-  value: z.string(),
-  count: z.number(),
-  percent: z.number(),
-  label: z.string()
-});
-
-const CleanJSONEntitySchema = z.object({
   value: z.string(),
   count: z.number(),
   percent: z.number(),
@@ -83,50 +71,15 @@ const ClickJsonGetAllSchema = z.object({
 });
 
 const FilterRepsonseSchema = z.object({
-  // filter: ClickFilterSchema.array(),
   chart: ClickChartSchema.array(),
   json: ClickJsonGetAllSchema,
 });
-
-const ServerResponseFilterSchema = serverResponseSchema(FilterRepsonseSchema);
-
-
-const FakeClickEventSchema = z.object({
-  id: z.number().nonnegative().lt(2_147_483_648),
-  linkId: z.number().nonnegative().lt(2_147_483_648),
-  source: z.enum(["qr", "link"]),
-  createdAt: z.string(),
-  country: z.string(),
-  countryCode: z.string(),
-  shortUrl: z.string(),
-  originalUrl: z.string(),
-  continent: z.string(),
-  region: z.string(),
-  city: z.string(),
-  latitude: z.number().gte(-90).lte(90),
-  longitude: z.number().gte(-180).lte(180)
-});
-
-const ServerResponseQuerySchema = serverResponseSchema(JSONEntitySchema.array());
 
 export const ComboboxJSONEntitySchema = z.object({
   value: z.string(),
   count: z.number(),
   label: z.string()
 });
-
-export const CleanComboboxJSONEntitySchema = z.object({
-  value: z.string(),
-  count: z.number(),
-  label: z.string()
-});
-
-export type ComboboxQuery = z.infer<typeof ComboboxJSONEntitySchema>;
-
-export const ServerResponseComboboxSchema = serverResponseSchema(CleanComboboxJSONEntitySchema.array());
-
-// I expect an array with 1 element who is a object with a single key called "data".
-// data has an object with 4 keys which are tabs, stats, combobox, and chart.
 
 const AnalyticsJSONSchema = z.object({
   data: z.object({
@@ -210,16 +163,16 @@ const AnalyticsJSONSchema = z.object({
 const AnalayticsOutputSchema = z.object({
   empty: z.boolean(),
   tabs: z.object({
-    source: CleanJSONEntitySchema.array(),
-    country: CleanJSONEntitySchema.array(),
-    region: CleanJSONEntitySchema.array(),
-    continent: CleanJSONEntitySchema.array(),
-    city: CleanJSONEntitySchema.array(),
-    shortUrl: CleanJSONEntitySchema.array(),
-    originalUrl: CleanJSONEntitySchema.array(),
-    browser: CleanJSONEntitySchema.array(),
-    device: CleanJSONEntitySchema.array(),
-    os: CleanJSONEntitySchema.array()
+    source: JSONEntitySchema.array(),
+    country: JSONEntitySchema.array(),
+    region: JSONEntitySchema.array(),
+    continent: JSONEntitySchema.array(),
+    city: JSONEntitySchema.array(),
+    shortUrl: JSONEntitySchema.array(),
+    originalUrl: JSONEntitySchema.array(),
+    browser: JSONEntitySchema.array(),
+    device: JSONEntitySchema.array(),
+    os: JSONEntitySchema.array()
   }),
   stats: z.object({
     numLinks: z.number(),
@@ -227,16 +180,16 @@ const AnalayticsOutputSchema = z.object({
     qrClicks: z.number()
   }),
   combobox: z.object({
-    source: CleanComboboxJSONEntitySchema.array(),
-    country: CleanComboboxJSONEntitySchema.array(),
-    region: CleanComboboxJSONEntitySchema.array(),
-    continent: CleanComboboxJSONEntitySchema.array(),
-    city: CleanComboboxJSONEntitySchema.array(),
-    shortUrl: CleanComboboxJSONEntitySchema.array(),
-    originalUrl: CleanComboboxJSONEntitySchema.array(),
-    browser: CleanComboboxJSONEntitySchema.array(),
-    device: CleanComboboxJSONEntitySchema.array(),
-    os: CleanComboboxJSONEntitySchema.array()
+    source: ComboboxJSONEntitySchema.array(),
+    country: ComboboxJSONEntitySchema.array(),
+    region: ComboboxJSONEntitySchema.array(),
+    continent: ComboboxJSONEntitySchema.array(),
+    city: ComboboxJSONEntitySchema.array(),
+    shortUrl: ComboboxJSONEntitySchema.array(),
+    originalUrl: ComboboxJSONEntitySchema.array(),
+    browser: ComboboxJSONEntitySchema.array(),
+    device: ComboboxJSONEntitySchema.array(),
+    os: ComboboxJSONEntitySchema.array()
   }),
   chart: z.object({
     date: z.date(),
@@ -246,21 +199,17 @@ const AnalayticsOutputSchema = z.object({
 });
 
 const ComboboxSchema = z.object({
-  source: CleanComboboxJSONEntitySchema.array(),
-  country: CleanComboboxJSONEntitySchema.array(),
-  region: CleanComboboxJSONEntitySchema.array(),
-  continent: CleanComboboxJSONEntitySchema.array(),
-  city: CleanComboboxJSONEntitySchema.array(),
-  shortUrl: CleanComboboxJSONEntitySchema.array(),
-  originalUrl: CleanComboboxJSONEntitySchema.array(),
-  browser: CleanComboboxJSONEntitySchema.array(),
-  device: CleanComboboxJSONEntitySchema.array(),
-  os: CleanComboboxJSONEntitySchema.array()
+  source: ComboboxJSONEntitySchema.array(),
+  country: ComboboxJSONEntitySchema.array(),
+  region: ComboboxJSONEntitySchema.array(),
+  continent: ComboboxJSONEntitySchema.array(),
+  city: ComboboxJSONEntitySchema.array(),
+  shortUrl: ComboboxJSONEntitySchema.array(),
+  originalUrl: ComboboxJSONEntitySchema.array(),
+  browser: ComboboxJSONEntitySchema.array(),
+  device: ComboboxJSONEntitySchema.array(),
+  os: ComboboxJSONEntitySchema.array()
 })
-
-export type ComboboxType = z.infer<typeof ComboboxSchema>;
-
-export const AnalyticsServerResponseSchema = serverResponseSchema(AnalayticsOutputSchema);
 
 export const RecordClickIfExistsSchema = ClickEventSchema.omit({
   id: true,
@@ -277,35 +226,29 @@ export const RecordClickIfExistsSchema = ClickEventSchema.omit({
     );
   });
 
+export const ServerResponseComboboxSchema = serverResponseSchema(ComboboxJSONEntitySchema.array());
+export const AnalyticsServerResponseSchema = serverResponseSchema(AnalayticsOutputSchema);
+
+export type ComboboxQuery = z.infer<typeof ComboboxJSONEntitySchema>;
+export type ComboboxType = z.infer<typeof ComboboxSchema>;
 export type RecordClickIfExistsSchemaType = z.infer<typeof RecordClickIfExistsSchema>;
 
 export namespace ClickEventSchemas {
-  export const Click = ClickEventSchema; // used
-  export const Create = ClickEventCreateSchema; // used
-  // export const GetAll = ClickEventGetAllSchema;
-  export const Fake = FakeClickEventSchema; // used
-  export const Filter = ClickFilterSchema; // used
-  export const Chart = ClickChartSchema; // used
-  export const ClickResponse = FilterRepsonseSchema;
-  export const ServerResponseFilter = ServerResponseFilterSchema; // used
-  export const ServerResponsQuery = ServerResponseQuerySchema; // used
-  export const JSONAgg = ClickJsonGetAllSchema; // used
+  export const Click = ClickEventSchema;
+  export const Create = ClickEventCreateSchema;
+  export const Filter = ClickFilterSchema;
+  export const Chart = ClickChartSchema;
+  export const JSONAgg = ClickJsonGetAllSchema;
   export const Query = JSONEntitySchema;
   export const AnalyticsJSON = AnalyticsJSONSchema;
 }
 
 export namespace ClickEventTypes {
-  export type Click = z.infer<typeof ClickEventSchema>; // used
-  export type Create = z.infer<typeof ClickEventCreateSchema>; // used
-  // export type GetAll = z.infer<typeof ClickEventGetAllSchema>;
-  export type Fake = z.infer<typeof FakeClickEventSchema>; // used
-  export type Filter = z.infer<typeof ClickFilterSchema>; // used
-  export type Chart = z.infer<typeof ClickChartSchema>; // used
-  export type ClickResponse = z.infer<typeof FilterRepsonseSchema>; // used
-  export type ServerResponseFilter = z.infer<typeof ServerResponseFilterSchema>;
-  export type ServerResponseQuery = z.infer<typeof ServerResponseQuerySchema>;
+  export type Click = z.infer<typeof ClickEventSchema>;
+  export type Create = z.infer<typeof ClickEventCreateSchema>;
+  export type Chart = z.infer<typeof ClickChartSchema>;
+  export type ClickResponse = z.infer<typeof FilterRepsonseSchema>;
   export type JSONAgg = z.infer<typeof ClickJsonGetAllSchema>;
-  export type Query = z.infer<typeof JSONEntitySchema>;
   export type AnalyticsJSON = z.infer<typeof AnalyticsJSONSchema>;
 }
 
