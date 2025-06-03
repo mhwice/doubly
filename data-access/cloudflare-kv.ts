1082aefe369860521a6bf82c9785fea07fafa2a1
Marshall Wice <mhwice@protonmail.com>
2025-06-03 12:27:29 -0700

    fix to kv key format

--- data-access/cloudflare-kv.ts
+++ data-access/cloudflare-kv.ts
@@ -6,7 +6,8 @@ const client = new Cloudflare({
 
 export async function writeToKV(code: string, originalUrl: string, linkId: number) {
   const key = code;
-  const payload = JSON.stringify({ originalUrl, linkId });
+  // const payload = JSON.stringify({ originalUrl, linkId });
+  const payload = `${originalUrl},${linkId}`;
   const namespaceId = "7fdaccaf9072443db29e72b452dd8254";
   const accountId = "cedc684260ce5373e68f79a5fa17e2f3";
   const metadata = "";
